a0326ebb160443ec77ce4e133c7be8afd69de55c
Jared Tanner <Deraj21@gmail.com>
2019-03-10 18:52:18 -0700

    installed react and dependancies

---
+++ log-viewer/src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        welcome to rachel-app
+      </div>
+    );
+  }
+}
+
+export default App;
